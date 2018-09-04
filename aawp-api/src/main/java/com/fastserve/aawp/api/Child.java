e97e72dea8fa0e3a3735d95d77b71c9116f4ebc8
AbhideepChakravarty <abhideepchakravarty@gmail.com>
2018-09-04 11:31:11 +0530

    New API added

---
+++ aawp-api/src/main/java/com/fastserve/aawp/api/Child.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fastserve.aawp.api;
+
+/**
+ *
+ * @author Admin
+ */
+public class Child extends Parent {
+    
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    
+}
